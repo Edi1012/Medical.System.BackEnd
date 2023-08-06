f98f007761b44f621a79831dfa31726665fed7bf
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-08-06 15:58:17 -0700

    add create user

--- Medical.System.BackEnd/Controllers/UserController.cs
+++ Medical.System.BackEnd/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Medical.System.Core.DTOs;
+using Medical.System.Core.Models.Catalogs;
+using Medical.System.Core.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace Medical.System.BackEnd.Controllers
 {
@@ -7,10 +10,42 @@ namespace Medical.System.BackEnd.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
+        public UserController(ICatalogsService catalogsService)
         {
-            return Ok("Hello World");
+            CatalogsService = catalogsService;
+        }
+
+        public ICatalogsService CatalogsService { get; }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] CreateUserDto userDto)
+        {
+            var user = new User
+            {
+                // Generate new ObjectId
+                Id = ObjectId.GenerateNewId().ToString(),
+                Name = userDto.Name,
+                Email = userDto.Email,
+                Password = userDto.Password, // remember to hash this before saving in a real scenario!
+            };
+
+            CatalogsService.CreateUserAsync(user);
+
+            // Return a 201 Created response, with the created user in the body, you can replace 'user' with a ReadUserDto if you want to hide the password.
+            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(string id)
+        {
+            var user = CatalogsService.GetUserByIdAsync(id);
+
+            if (user == null)
+        {
+                return NotFound();
+            }
+
+            return Ok(user);
         }
     }
 }
